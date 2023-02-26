1159d58df6e6245c084539ea4396f4944286549b
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-02-26 01:52:09 -0800

    Sign up and Log in Update

--- lib/User_Authentication/Log-In.dart
+++ lib/User_Authentication/Log-In.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LogIn extends StatefulWidget {
   const LogIn({Key? key}) : super(key: key);
@@ -10,6 +11,20 @@ class LogIn extends StatefulWidget {
 class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(35),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(height: 30),
+            Image.asset("assets/Logos/TC_Logo4.png"),
+            SizedBox(height: 5),
+
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/User_Authentication/Sign-Up.dart
+++ lib/User_Authentication/Sign-Up.dart
@@ -10,6 +10,20 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(35),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(height: 30),
+            Image.asset("assets/Logos/TC_Logo4.png"),
+            SizedBox(height: 5),
+
+          ],
+        ),
+      ),
+    );
   }
 }
