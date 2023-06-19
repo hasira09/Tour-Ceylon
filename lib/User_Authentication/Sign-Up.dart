1702c95471cc9d2745fd5dab0260262a6f9cbc70
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-20 00:51:51 +0530

    Update Sign-Up.dart

--- lib/User_Authentication/Sign-Up.dart
+++ lib/User_Authentication/Sign-Up.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../Components/black_button.dart';
+import '../Components/my_textfield.dart';
+import '../Components/square_tile.dart';
+import 'Log-In.dart';
+
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
 
@@ -8,18 +13,133 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+
+  // text editing controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final passwordConfirmController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(35),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          //mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(height: 30),
-            Image.asset("assets/Logos/TC_Logo4.png"),
-            SizedBox(height: 5),
+            SizedBox(height: 50,),
+            Container(
+                height: 165,
+                width: 205,
+                child: Image.asset('assets/Logos/TCLogo.png')),
+
+            SizedBox(height: 25,),
+
+            MyTextField(
+              controller: emailController,
+              hintText: 'Email',
+              obscureText: false,
+              suffixIcon: Icon(Icons.person_3_outlined,color: Color.fromRGBO(255, 57, 29, 1)),
+            ),
+
+            SizedBox(height: 20,),
+
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: false,
+              suffixIcon: Icon(Icons.password_outlined,color: Color.fromRGBO(255, 57, 29, 1)),
+            ),
+
+            SizedBox(height: 20,),
+
+            MyTextField(
+              controller: passwordConfirmController,
+              hintText: 'Confirm Password',
+              obscureText: false,
+              suffixIcon: Icon(Icons.password_outlined,color: Color.fromRGBO(255, 57, 29, 1)),
+            ),
+
+            SizedBox(height: 15,),
+
+            BlackButton(
+              onTap: () {},
+              childText: 'Sign Up',
+            ),
+
+            Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Divider(
+                        height: 65,
+                        thickness: 2,
+                        endIndent: 10,
+                        indent: 30,
+                        color: Colors.grey.shade400,
+                      )
+                  ),
+
+                  Text("Or"),
+
+                  Expanded(
+                      child: Divider(
+                        height: 65,
+                        thickness: 2,
+                        indent: 10,
+                        endIndent: 30,
+                        color: Colors.grey.shade400,
+                      )
+                  ),
+                ]
+            ),
+
+
+
+            // google + apple sign in buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                // google button
+                SquareTile(imagePath: 'assets/images/Google.png'),
+
+                SizedBox(width: 15),
+
+                // apple button
+                SquareTile(imagePath: 'assets/images/Facebook.png'),
+
+                SizedBox(width: 15),
+
+                // IG button
+                SquareTile(imagePath: 'assets/images/Instagram.png')
+              ],
+            ),
+
+            const SizedBox(height: 10),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Already Have an Account?",
+                  style: TextStyle(fontSize: 17, color: Colors.black),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LogIn()));
+                  },
+                  child: Text(
+                    " Login",
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Color.fromRGBO(255, 57, 29, 1)),
+                  ),
+                ),
+              ],
+            ),
+
+
+
+
 
           ],
         ),
